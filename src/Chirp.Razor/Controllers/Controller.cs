d957317b3e8244d16fdbfde2290a23c7fcf419ba
Phillip <phir@itu.dk>
2026-02-10 11:03:03 +0100

    Added all the endpoints we need

--- src/Chirp.Razor/Controllers/Controller.cs
+++ src/Chirp.Razor/Controllers/Controller.cs
@@ -3,8 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 namespace Chirp.Razor.Controllers;
 
 [ApiController]
-[Route("/register")]
 public class Controller : ControllerBase
 {
-    
+    [HttpGet("/fllws/{username}")]                 
+    public IActionResult Follows(string username)
+    {
+        
+    } 
+    [HttpPost("/fllws/{username}")]                 
+    public IActionResult Follow(string username)
+    {
+        
+    }
+    [HttpGet("/latest")]                 
+    public IActionResult Latest()
+    {
+        
+    }
+    [HttpGet("/msgs")]                 
+    public IActionResult RecentMessages()
+    {
+        
+    }
+    [HttpGet("/msgs/{username}")]                 
+    public IActionResult MessagesByUser(string username)
+    {
+        
+    }
+    [HttpPost("/msgs/{username}")]                 
+    public IActionResult PostNewMessage(string username)
+    {
+        
+    }  
+    [HttpPost("/register")]              
+    public IActionResult Register()
+    {
+        
+    }  
 }
