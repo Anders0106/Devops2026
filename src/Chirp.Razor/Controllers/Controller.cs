a947863bbd527f8cb4cf757a317bd334e88570f9
Alex Op <alexop1000@gmail.com>
2026-02-10 11:05:23 +0100

    Add db context and fix errors in controller

--- src/Chirp.Razor/Controllers/Controller.cs
+++ src/Chirp.Razor/Controllers/Controller.cs
@@ -1,3 +1,4 @@
+using Chirp.Repositories.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.Razor.Controllers;
@@ -5,39 +6,46 @@ namespace Chirp.Razor.Controllers;
 [ApiController]
 public class Controller : ControllerBase
 {
+	private readonly ChirpDBContext _context;
+	private readonly IServiceProvider _provider;
+	public Controller(ChirpDBContext context, IServiceProvider provider)
+	{
+		_context = context;
+		_provider = provider;
+	}
     [HttpGet("/fllws/{username}")]                 
     public IActionResult Follows(string username)
     {
-        
+        return Ok();
     } 
     [HttpPost("/fllws/{username}")]                 
     public IActionResult Follow(string username)
     {
-        
+        return Ok();    
     }
     [HttpGet("/latest")]                 
     public IActionResult Latest()
     {
-        
+        return Ok();
     }
     [HttpGet("/msgs")]                 
     public IActionResult RecentMessages()
     {
-        
+        return Ok();
     }
     [HttpGet("/msgs/{username}")]                 
     public IActionResult MessagesByUser(string username)
     {
-        
+        return Ok();
     }
     [HttpPost("/msgs/{username}")]                 
     public IActionResult PostNewMessage(string username)
     {
-        
+        return Ok();
     }  
     [HttpPost("/register")]              
     public IActionResult Register()
     {
-        
+        return Ok();
     }  
 }
