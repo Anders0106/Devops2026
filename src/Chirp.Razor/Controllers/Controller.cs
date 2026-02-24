518bc3b27d554c1ebce2321d4e05fe8331d1fd9f
Alex Op <alexop1000@gmail.com>
2026-02-24 10:13:49 +0100

    Fix register route

--- src/Chirp.Razor/Controllers/Controller.cs
+++ src/Chirp.Razor/Controllers/Controller.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Text.Json.Serialization;
 
 namespace Chirp.Razor.Controllers;
 
@@ -227,6 +228,7 @@ public class Controller : ControllerBase
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [JsonPropertyName("pwd")]
         public string Password { get; set; }
     }
 
